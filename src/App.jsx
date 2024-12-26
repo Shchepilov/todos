f0e29ebd8af3870888e86e98ba26d44f0cb5e037
Ihor Shchepilov <shchepilov.igor@gmail.com>
2024-12-26 21:11:51 +0200

    Firebase init

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import styles from './App.module.css'
+import Auth from './Auth';
 
 const App = () => {
   return (
     <div className={styles.App}>
+      <header>
+        <Auth />
+      </header>
       App
     </div>
   );

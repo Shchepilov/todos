f81bb9f4eefc759a367c739d55a7478bb9954453
Ihor Shchepilov <shchepilov.igor@gmail.com>
2025-02-02 23:54:51 +0200

    Refactoring forms

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -1,6 +1,7 @@
 import { useState, useRef } from "react";
 import { useStore } from "../../store/store";
 import styles from "./TodoForm.module.scss";
+import forms from "../../Forms.module.scss";
 import dayjs from "dayjs";
 
 const TodoForm = () => {
@@ -13,7 +14,9 @@ const TodoForm = () => {
     const [dueDate, setDueDate] = useState(currentDate);
     const [isDueDate, setIsDueDate] = useState(false);
 
-    const handleAddTodo = () => {
+    const handleAddTodo = (e) => {
+        e.preventDefault();
+
         const content = contentRef.current.value;
         const priority = priorityRef.current.value;
         const dueDate = dueDateRef.current.value;
@@ -27,28 +30,39 @@ const TodoForm = () => {
     };
 
     return (
-        <form onSubmit={(e) => e.preventDefault()} className={styles.form}>
-            <div className={styles.formRow}>
-                <input
-                    type="text"
-                    ref={contentRef}
-                    placeholder="Add a new todo"
-                />
+        <form onSubmit={(e) => e.preventDefault()} className={styles.form + " " + forms.form}>
+            <div className={forms.formLabel}>
+                <label>Title</label>
+            </div>
+
+            <div className={forms.formField}>
+                <input type="text" ref={contentRef} placeholder="Add a new todo"/>
+            </div>
+
+            <div className={forms.formLabel}>
+                <label>Priority</label>
+            </div>
+
+            <div className={forms.formField}>
                 <select ref={priorityRef}>
-                <option value="" disabled>Select priority</option>
+                    <option value="" disabled>Select priority</option>
                     <option value="low">Low</option>
                     <option value="medium">Medium</option>
                     <option value="high">High</option>
                 </select>
             </div>
 
-            <button onClick={handleAddTodo}>Add</button>
-
-            <div className={styles.formRow}>
+            <div className={forms.formLabel}>
+                <label>Due date</label>
+            </div>
+            
+            <div className={forms.formField}>
+                <input type="date" disabled={!isDueDate} ref={dueDateRef} value={dueDate} onChange={(e) => setDueDate(e.target.value)} />
                 <input type="checkbox" checked={isDueDate} onChange={(e) => setIsDueDate(e.target.checked)} />
-                <label>du date</label>
+            </div>
 
-                {isDueDate && <input type="date" ref={dueDateRef} value={dueDate} onChange={(e) => setDueDate(e.target.value)} />}
+            <div className={forms.emptyLabel}>
+                <button className={forms.button} onClick={handleAddTodo}>Add</button>
             </div>
         </form>
     );

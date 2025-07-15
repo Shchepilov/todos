978f567d814f5e7594db3e1afa98b85544ec0668
Ihor Shchepilov <shchepilov.igor@gmail.com>
2025-07-16 00:30:29 +0300

    Todos: refactoring Todo forms

--- src/features/todos/components/TodoItem/EditForm.jsx
+++ src/features/todos/components/TodoItem/EditForm.jsx
@@ -1,84 +1,90 @@
-import { useState, useRef, useId } from "react";
+import { useState, useRef } from "react";
 import { Dialog } from "radix-ui";
+import { useForm } from "react-hook-form"
+import * as Form from '@radix-ui/react-form';
 import dayjs from "dayjs";
 import { useStore } from "@store/store";
 import Checkbox from "@components/Checkbox/Checkbox";
+import Input from "@components/Input/Input";
+import Select from "@components/Select/Select";
+import Field from "@components/Field/Field";
 import Button from "@components/Button/Button";
 import { PRIORITY_OPTIONS } from "@features/todos/utils/constants";
 
 const EditForm = ({ id, content, priority, date, dueDate, autoMove, handleUpdate }) => {
+    const { register, handleSubmit, formState: { errors }, } = useForm();
+
     const currentDay = useStore((state) => state.currentDay);
     const currentDate = dayjs(currentDay).format("YYYY-MM-DD");
-    const [newDueDate, setNewDueDate] = useState(dueDate || currentDate);
     const [isDueDate, setIsDueDate] = useState(!!dueDate);
-    const [newContent, setNewContent] = useState(content);
-    const [newPriority, setNewPriority] = useState(priority);
-    const [newDate, setNewDate] = useState(date);
     const [newAutoMove, setNewAutoMove] = useState(autoMove);
-    const priorityId = useId();
-    const dueDateId = useId();
-    const dateId = useId();
+    
     const closeDialogRef = useRef(null);
 
-    const handleChangeContent = (e) => setNewContent(e.target.value);
-    const handleChangePriority = (e) => setNewPriority(e.target.value);
-    const handleChangeDate = (e) => setNewDate(e.target.value);
-    const handleChangeDueDate = (e) => setNewDueDate(e.target.value);
     const handleIsDueDate = (e) => setIsDueDate(e.target.checked);
     const handleChangeAutoMove = (e) => setNewAutoMove(e.target.checked);
     const handleCloseForm = () => closeDialogRef.current?.click()
-    const handleSubmitForm = (e) => e.preventDefault();
 
-    const handleUpdateTodo = (e) => {
-        e.preventDefault();
-
-        if (!newContent) return;
-
-        const updatedDueDate = isDueDate ? newDueDate : null;
-
-        handleUpdate(id, newContent, newPriority, newDate, updatedDueDate, newAutoMove);
+    const handleUpdateTodo = (data) => {
+        const { todoTitle, todoPriority, todoDueDate, todoDate } = data;
+        const updatedDueDate = isDueDate ? todoDueDate : null;
+        
+        handleUpdate(id, todoTitle, todoPriority, todoDate, updatedDueDate, newAutoMove);
         handleCloseForm();
     };
 
     return (
-        <form className="form" onSubmit={handleSubmitForm}>
-            <div className="row">
-                <div className="field">
-                    <input
-                        type="text"
-                        value={newContent}
-                        onChange={handleChangeContent}
-                        placeholder="Add a new todo"
-                    />
-                </div>
-            </div>
+        <Form.Root onSubmit={handleSubmit(handleUpdateTodo)} className="form">
+            <Field name="todoTitle" label="Title" errors={errors}>
+                <Input
+                    register={register}
+                    name="todoTitle"
+                    label="Title"
+                    placeholder="Title"
+                    autoFocus
+                    errors={errors}
+                    required="Field is required"
+                    defaultValue={content}
+                    maxLength={{
+                        value: 40,
+                        message: "Title cannot exceed 40 characters"
+                    }}
+                />
+            </Field>
+
+            <Field name="todoPriority" label="Priority" errors={errors}>
+                <Select
+                    register={register}
+                    name="todoPriority"
+                    nameKey="label"
+                    items={PRIORITY_OPTIONS}
+                    defaultValue={priority}
+                />
+            </Field>
 
             <div className="row">
-                <div className="field">
-                    <label htmlFor={priorityId} className="label">Priority</label>
-                    <select value={newPriority} id={priorityId} onChange={handleChangePriority}>
-                        {PRIORITY_OPTIONS.map(option => (
-                            <option key={option.value} value={option.value}>
-                                {option.label}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-            </div>
+                <Field name="todoDate" label="Date" errors={errors}>
+                    <Input
+                        type="date"
+                        register={register}
+                        name="todoDate"
+                        defaultValue={date}
+                        errors={errors}/>
+                </Field>
 
-            <div className="row">
-                <div className="field">
-                    <label htmlFor={dateId} className="label">Date</label>
-                    <input type="date" id={dateId} value={newDate} onChange={handleChangeDate} />
-                </div>
-
-                <div className="field">
-                    <label htmlFor={dueDateId} className="label">Due Date</label>
-
-                    <div className="field split-field">
-                        <input type="date" id={dueDateId} disabled={!isDueDate} value={newDueDate} onChange={handleChangeDueDate} />
-                        <Checkbox type="checkbox" checked={isDueDate} onChange={handleIsDueDate} />
-                    </div>    
+                <div className="field split-field">
+                    <Field name="todoDueDate" label="Due Date" errors={errors}>
+                        <Input
+                            type="date"
+                            register={register}
+                            name="todoDueDate"
+                            defaultValue={dueDate || currentDate}
+                            errors={errors}
+                            disabled={!isDueDate}
+                        />
+                    </Field>
+                    
+                    <Checkbox checked={isDueDate} onChange={handleIsDueDate}/>
                 </div>
             </div>
 
@@ -90,11 +96,11 @@ const EditForm = ({ id, content, priority, date, dueDate, autoMove, handleUpdate
 
             <div className="button-group">
                 <Button variation="secondary" onClick={handleCloseForm}>Cancel</Button>
-                <Button onClick={handleUpdateTodo} disabled={!newContent}>Update</Button>
+                <Button type="submit">Update</Button>
             </div>
 
             <Dialog.Close ref={closeDialogRef} hidden></Dialog.Close>
-        </form>
+        </Form.Root>
     );
 };
 

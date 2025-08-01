f85f1135d88a404bd104cb5d6d8c5c5ea3c1c70e
Ihor Shchepilov <shchepilov.igor@gmail.com>
2025-08-01 19:18:40 +0300

    Refactoring modal window position

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -9,18 +9,16 @@ const Modal = ({ heading, align='center', size='small', isDialogOpen, setIsDialo
             <Dialog.Portal>
                 <Dialog.Overlay className={modal.backdrop} />
                 <div className={modal.wrapper}>
-                <Dialog.Content className={`${modal.content} ${modal[align]} ${modal[size]}`} aria-describedby={undefined}>
-                    
-                    <Dialog.Title className={modal.title}>{heading}</Dialog.Title>
-                    <Dialog.Close asChild>
-                        <button className={modal.close} aria-label="Close">
-                            <Cross2Icon />
-                        </button>
-                    </Dialog.Close>
-                    
-                    {children}
-                    
-                </Dialog.Content>
+                    <Dialog.Content className={`${modal.content} ${modal[align]} ${modal[size]}`} aria-describedby={undefined}>
+                        <Dialog.Title className={modal.title}>{heading}</Dialog.Title>
+                        <Dialog.Close asChild>
+                            <button className={modal.close} aria-label="Close">
+                                <Cross2Icon />
+                            </button>
+                        </Dialog.Close>
+                        
+                        {children}
+                    </Dialog.Content>
                 </div>
                 
             </Dialog.Portal>
